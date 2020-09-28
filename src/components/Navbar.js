fec47d01543e22e2751f69855a2039ec8de64b68
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-09-29 01:39:04 +0200

    Fix on logo click bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar(){
         <>
         <nav className="navbar">
             <div className='navbar-container'>
-                <Link to="/" className="navbar-logo">
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                     TRVL <i className="fab fa-typo3"/>
                  </Link>
                  <div className="menu-icon" onClick={handleClick}>
