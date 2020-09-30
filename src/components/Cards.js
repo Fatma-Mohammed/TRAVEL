ecaadca587779d9297688da0860be4b830b750f6
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-09-30 21:04:17 +0200

    change cards redirection paths

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,13 +13,13 @@ function Cards() {
                         src='imgs/img-9.jpg'
                         text='Explore the hidden waterfall deep inside the Amazon Jungle'
                         label='Adventure'
-                        path='/services'
+                        path='/'
                         />
                         <CardItem
                         src='imgs/img-2.jpg'
                         text='Travel through the Islands of Bali in a Private Cruise'
                         label='Luxury'
-                        path='/services'
+                        path='/'
                         />
                     </ul>
                     <ul className='cards__items'>
@@ -27,19 +27,19 @@ function Cards() {
                         src='imgs/img-3.jpg'
                         text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
                         label='Mystery'
-                        path='/services'
+                        path='/'
                         />
                         <CardItem
                         src='imgs/img-4.jpg'
                         text='Experience Football on Top of the Himilayan Mountains'
                         label='Adventure'
-                        path='/products'
+                        path='/'
                         />
                         <CardItem
                         src='imgs/img-8.jpg'
                         text='Ride through the Sahara Desert on a guided camel tour'
                         label='Adrenaline'
-                        path='/sign-up'
+                        path='/'
                         />
                     </ul>
                 </div> 
