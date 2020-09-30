d2fd9e6bf32fe27f04a0de44d04474cdb51506aa
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-09-30 19:24:37 +0200

    Add cards & cards style

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import Hero from '../Hero';
+import Cards from '../Cards';
 
 function Home() {
     return(
         <>
             <Hero/>
+            <Cards/>
         </>
     )
 }
